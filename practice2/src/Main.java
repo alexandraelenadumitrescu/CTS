15b28c65dfa621674f11ddb0c25d5d8e1fafdbba
alexandraelenadumitrescu <134046146+alexandraelenadumitrescu@users.noreply.github.com>
2026-02-22 16:52:31 +0200

    practice 2 clase abstracte si interfete

---
+++ practice2/src/Main.java
@@ -0,0 +1,106 @@
+//Since we just covered the theory, I’ve prepared a scenario
+// for you. You are designing a system for a Smart Home.
+// You have two types of objects: Electronic Devices (like a TV)
+// and Appliances (like a Toaster).
+//
+//The Challenge
+//I want you to write the Java code for this scenario
+// using both an Abstract Class and an Interface.
+//
+//The Interface: Create an interface called RemoteControllable.
+// It should have a method void pressPowerButton().
+// (Many different things can be remote-controlled, even if they aren't related).
+//
+//The Abstract Class: Create an abstract class
+// called ElectronicDevice. It should have a protected
+// variable int batteryLevel and a concrete method void
+// showBattery() that prints the level.
+//
+//The Concrete Class: Create a class SmartTV that
+// extends the abstract class and implements the interface.
+//
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        SmartTV smartTV=new SmartTV();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();
+        smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();smartTV.pressButton();
+        smartTV.showBattery();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
